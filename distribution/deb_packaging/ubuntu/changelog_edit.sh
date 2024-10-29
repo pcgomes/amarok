f83c6ec742d0d1772db570bca0e49fb649127e0e
Pedro de Carvalho Gomes <pedrogomes81@gmail.com>
2024-10-29 20:34:12 +0100

    Release of version 3.1.1

--- distribution/deb_packaging/ubuntu/changelog_edit.sh
+++ distribution/deb_packaging/ubuntu/changelog_edit.sh
@@ -3,18 +3,16 @@
 DATENOW_TIMESTAMP=$(date "+%s");
 DATENOW_YMD_HMSZ=$(date -d "@${DATENOW_TIMESTAMP}" +"%Y%m%d%H%M%S%z")
 declare -A VERSION_CODENAME
-VERSION_CODENAME[18.04]="bionic"
-VERSION_CODENAME[20.04]="focal"
 VERSION_CODENAME[22.04]="jammy"
-VERSION_CODENAME[23.10]="mantic"
-VERSION_CODENAME[24.04]="lunar"
+VERSION_CODENAME[24.04]="noble"
+VERSION_CODENAME[24.10]="oracular"
 
 COMMENT=${1:-Release of v${DATENOW_YMD_HMSZ}}
 
 for version in ${!VERSION_CODENAME[@]}; do
   code=${VERSION_CODENAME[${version}]}
   cat << EOF > ${version}/new_cl_entry;
-amarok (3:3.0.0+1SNAPSHOT${DATENOW_YMD_HMSZ}-0ubuntu1ppa1~ubuntu${version}.1) ${code}; urgency=medium
+amarok (3:3.1.1+1SNAPSHOT${DATENOW_YMD_HMSZ}-0ubuntu1ppa1~ubuntu${version}.1) ${code}; urgency=medium
 
   * ${COMMENT}
 
